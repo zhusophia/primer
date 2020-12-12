1e01ed9ba6991b6a6610fafbd6afd57ef48c4134
David Kane <dave.kane@gmail.com>
2020-12-12 14:11:39 -0500

    more comments

--- book_start.R
+++ book_start.R
@@ -18,24 +18,15 @@ library(gifski)
 
 options(digits = 2)
 
-# Without this, the cache is created --- in book_temp_cache, since
+# Without this next line, the cache is created --- in book_temp_cache, since
 # `book_filename` is set to "book_temp" in _bookdown.yml --- and then
 # automatically deleted by bookdown after knitting is complete. We want the
 # cache to persist, at least in the book-builders home directory. I don't think
 # that using a common directory will cause conflicts across chapters . . .
 
-# Critical question is whether or not to use/require cache and, if so, where. I
-# am concerned that the cache stuff may cause weird errors when people are just
-# trying to knit their chapters. That makes me nervous . . . And, it has caused
-# me enough problems that I have just turned it off for everyone for now. Or are
-# the problems really caused by the fact that there is a directory --- usually
-# junk/ --- where everything gets dumped? Certainly, if there is stuff in junk
-# it ends up automatically linked to whatever individual chapter you are
-# knitting.
-
-# knitr::opts_chunk$set(cache=TRUE)
 knitr::opts_chunk$set(cache.path = "cache-directory/")
 
-# Question: Should I delete the cache each time I build the book for release?
+# I experimented with setting cache = TRUE for all code chunks by this is too
+# dangerous/confusing. Just cache the chunks which take a lot of time.
 
 
