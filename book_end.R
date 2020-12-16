0a0f7e5a0eea65a7a43bc7a517bc65e4faf87552
David Kane <dave.kane@gmail.com>
2020-12-15 22:56:01 -0500

    part way to get rid of renv::snapshot here.

--- book_end.R
+++ book_end.R
@@ -1,12 +1,12 @@
 # Create a bibliography with all the packages we are using.
 
 # It would be nice if this only ran when we are building the whole book. How can
-# we check that?
+# we check that? In the meantime, the fact that this runs all the time mean that
+# we need to add bib/packages to .gitignore. If we did not then every time
+# someone knitted a chapter, they would need to remember not to check in a new
+# version of bib/packages.bib.
 
 knitr::write_bib(c(.packages(), "bookdown"), "bib/packages.bib")
 
-# Running snapshot from renv after a successful build to update necessary
-# packages. This used to work fine without the force argument, but started
-# failing, at least with chapter 6, with a message about "pre-flight validation failure".
-
-# renv::snapshot(force = TRUE)
+# Use to run renv::snapshot() here. But that is a bad idea for reasons discussed
+# above with regard to bib/packages.bib.
